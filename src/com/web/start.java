a960804508a384baa6b4007bf3bd5ab6f72e809e
Tuuz <miracle-plus@outlook.com>
2020-10-23 14:51:22 +0800

    Tuuz

--- src/com/web/start.java
+++ src/com/web/start.java
@@ -1,5 +1,21 @@
 package com.web;
 
-public class start {
-    
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class start extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        System.out.println("客户端发起了post请求");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("客户端发起了get请求");
+    }
 }
